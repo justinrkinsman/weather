d5f20e66cf244a520cca8458f37f75d7138917cd
Justin <justinrkinsman@gmail.com>
2022-07-22 13:38:25 -0300

    Add functionality to city search box

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,10 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Halifax&APPID=da9add490059445074b78ded2c4fa995', {mode: 'cors'})
+let city = document.getElementById('city')
+
+document.getElementById('btn').addEventListener('click', () => {
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&APPID=da9add490059445074b78ded2c4fa995`, {mode: 'cors'})
     .then(function(response){
         return response.json();
     })
@@ -11,7 +14,12 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Halifax&APPID=da9add490
         console.log(response)
         console.log(response.name + ", " + response.sys.country)
         console.log(`Current Temp: ${response.main.temp - 273.15}`)
+        console.log(`Feels like: ${response.main.feels_like - 273.15}`)
+        console.log(`Current Weather Conditions: ${response.weather[0].main}`)
+        console.log(`Sunrise Time: ${new Date(response.sys.sunrise*1000)}`)
+        console.log(`Sunset Time: ${new Date(response.sys.sunset*1000)}`)
     })
+})
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Halifax&APPID=da9add490059445074b78ded2c4fa995', {mode: 'cors'})
+let city = document.getElementById('city')
+
+document.getElementById('btn').addEventListener('click', () => {
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&APPID=da9add490059445074b78ded2c4fa995`, {mode: 'cors'})
     .then(function(response){
         return response.json();
     })
@@ -6,4 +9,9 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Halifax&APPID=da9add490
         console.log(response)
         console.log(response.name + ", " + response.sys.country)
         console.log(`Current Temp: ${response.main.temp - 273.15}`)
+        console.log(`Feels like: ${response.main.feels_like - 273.15}`)
+        console.log(`Current Weather Conditions: ${response.weather[0].main}`)
+        console.log(`Sunrise Time: ${new Date(response.sys.sunrise*1000)}`)
+        console.log(`Sunset Time: ${new Date(response.sys.sunset*1000)}`)
     })
+})
