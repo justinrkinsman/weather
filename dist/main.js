4b00fdd7b4f6689844b0e42399adfe4e3f56a85b
Justin <justinrkinsman@gmail.com>
2022-07-22 12:14:36 -0300

    Link css and js to html

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,7 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-
+document.querySelector('button').addEventListener('click', () => console.log('Hi'))
 /******/ })()
 ;
+//# sourceMappingURL=main.js.map
