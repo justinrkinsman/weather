82a8f4dadf759cbc039704cbd3b12c9fb7ca3cd4
Justin <justinrkinsman@gmail.com>
2022-07-22 12:29:01 -0300

    Console.log weather from fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-document.querySelector('button').addEventListener('click', () => console.log('Hi'))
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Halifax&APPID=da9add490059445074b78ded2c4fa995', {mode: 'cors'})
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response)
+    })
