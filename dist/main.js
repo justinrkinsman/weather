f8384d232a8a3f06eaea180862fb5565ee2c5395
Justin <justinrkinsman@gmail.com>
2022-07-22 14:19:00 -0300

    Add weather details to dom

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,30 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+window.addEventListener('load', () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Bathurst,CA&APPID=da9add490059445074b78ded2c4fa995`, {mode: 'cors'})
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response) {
+        document.getElementById("cityName").textContent = response.name + ", " + response.sys.country
+        document.getElementById("currentTemp").textContent = `Current Temp: ${response.main.temp - 273.15} \u00b0 C`
+        document.getElementById('feelsLike').textContent = `Feels Like: ${response.main.feels_like - 273.15}`
+        document.getElementById('currentWeather').textContent = `Current Weather Conditions: ${response.weather[0].main}`
+        document.getElementById('sunrise').textContent = `Sunrise Time: ${new Date(response.sys.sunrise*1000)}`
+        document.getElementById('sunset').textContent = `Sunset Time: ${new Date(response.sys.sunset*1000)}`
+    /*    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=T9MZvkuHdTIWdpKEiUvT8fur5A2JESV1&s=${response.weather[0].main}`, {mode: 'cors'})
+.then(function(response) {
+    return response.json();
+})
+.then(function(response) {
+    img.src = response.data.images.original.url
+})*/
+})
+})
+
 let city = document.getElementById('city')
+let img = document.getElementById('image')
 
 document.getElementById('btn').addEventListener('click', () => {
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&APPID=da9add490059445074b78ded2c4fa995`, {mode: 'cors'})
@@ -11,14 +34,20 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&APPID=da9
         return response.json();
     })
     .then(function(response) {
-        console.log(response)
-        console.log(response.name + ", " + response.sys.country)
-        console.log(`Current Temp: ${response.main.temp - 273.15}`)
-        console.log(`Feels like: ${response.main.feels_like - 273.15}`)
-        console.log(`Current Weather Conditions: ${response.weather[0].main}`)
-        console.log(`Sunrise Time: ${new Date(response.sys.sunrise*1000)}`)
-        console.log(`Sunset Time: ${new Date(response.sys.sunset*1000)}`)
-    })
+        document.getElementById("cityName").textContent = response.name + ", " + response.sys.country
+        document.getElementById("currentTemp").textContent = `Current Temp: ${response.main.temp - 273.15} \u00b0 C`
+        document.getElementById('feelsLike').textContent = `Feels Like: ${response.main.feels_like - 273.15}`
+        document.getElementById('currentWeather').textContent = `Current Weather Conditions: ${response.weather[0].main}`
+        document.getElementById('sunrise').textContent = `Sunrise Time: ${new Date(response.sys.sunrise*1000)}`
+        document.getElementById('sunset').textContent = `Sunset Time: ${new Date(response.sys.sunset*1000)}`
+      /*  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=T9MZvkuHdTIWdpKEiUvT8fur5A2JESV1&s=${response.weather[0].main}`, {mode: 'cors'})
+.then(function(response) {
+    return response.json();
+})
+.then(function(response) {
+    img.src = response.data.images.original.url
+})*/
+})
 })
 /******/ })()
 ;
